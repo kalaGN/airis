4ac9e1202e820adf4876e7613b051f9760a67fc3
afei <491010750@qq.com>
2022-07-18 10:56:28 +0800

    api demo

--- routes/api.go
+++ routes/api.go
@@ -2,7 +2,7 @@
  * @Author: afei
  * @Date: 2022-07-14 09:22:45
  * @LastEditors: afei
- * @LastEditTime: 2022-07-14 14:23:48
+ * @LastEditTime: 2022-07-18 10:55:38
  * @Description:
  *
  * Copyright (c) 2022 , All Rights Reserved.
@@ -10,6 +10,10 @@
 package routes
 
 import (
+	"math/rand"
+	"strconv"
+	"time"
+
 	"github.com/kataras/iris/v12"
 )
 
@@ -19,13 +23,18 @@ func RegisterAPIRoutes(app *iris.Application) {
 	// Simple group: v1
 	v1 := app.Party("/v1")
 	{
-		v1.Get("/login", list)
+		v1.Get("/login", demo)
 	}
 
 	// Simple group: ivr
 	ivr := app.Party("/ivr")
 	{
 		ivr.Get("/inter/create", demo)
+		ivr.Get("/inter/getflowname", getflowname)
+
+		ivr.Post("/inter/copy", copy)
+		ivr.Post("/inter/deleteprocess", del)
+
 	}
 }
 
@@ -34,26 +43,29 @@ type CommonRes struct {
 	Id   string `json:"id"`
 }
 
+type CommonResg struct {
+	Code string `json:"code"`
+	Name string `json:"name"`
+}
+
 func demo(ctx iris.Context) {
-	d1 := CommonRes{"0", "wf000001"}
+	rand.Seed(time.Now().UnixNano())
+
+	d1 := CommonRes{"0", "cret0000" + strconv.Itoa(rand.Intn(10000))}
 	ctx.JSON(d1)
 }
 
-// Book example.
-type Book struct {
-	Title string `json:"title"`
+func getflowname(ctx iris.Context) {
+	rand.Seed(time.Now().UnixNano())
+	d1 := CommonResg{"0", "name0000" + strconv.Itoa(rand.Intn(10000))}
+	ctx.JSON(d1)
 }
-
-func list(ctx iris.Context) {
-	books := []Book{
-		{"Mastering Concurrency in Go"},
-		{"Go Design Patterns"},
-		{"Black Hat Go"},
-	}
-
-	ctx.JSON(books)
-	// TIP: negotiate the response between server's prioritizes
-	// and client's requirements, instead of ctx.JSON:
-	// ctx.Negotiation().JSON().MsgPack().Protobuf()
-	// ctx.Negotiate(books)
+func copy(ctx iris.Context) {
+	rand.Seed(time.Now().UnixNano())
+	d1 := CommonRes{"0", "copy0000" + strconv.Itoa(rand.Intn(10000))}
+	ctx.JSON(d1)
+}
+func del(ctx iris.Context) {
+	d1 := CommonRes{"200", "ok"}
+	ctx.JSON(d1)
 }
